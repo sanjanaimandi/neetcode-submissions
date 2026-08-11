64390a2e68e7304a9767cce2aec72ed0d1f3b4e6
sanjanaimandi <117230060+sanjanaimandi@users.noreply.github.com>
2026-08-11 17:03:51 -0400

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+      dicti = {}
+      for word in strs:
+        key = tuple(sorted(word))
+        if key not in dicti:
+            dicti[key] = []
+        dicti[key].append(word)
+
+      return list(list(dicti.values()))
+
+
+
+    
+    
+    
+
+
